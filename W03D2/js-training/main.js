b258f0993e6545694e82dfa829ff0b330dadf80d
oleksandralaskaj <alexandravalko@gmail.com>
2024-05-14 11:59:07 +0200

    add whether odd block

--- W03D2/js-training/main.js
+++ W03D2/js-training/main.js
@@ -8,7 +8,7 @@ const makeNewSectionWithButton = (h1, p, id) => {
     body.appendChild(section)
 }
 
-///weather
+///Weather
 const id1 = "weather"
 makeNewSectionWithButton("Weather", "Write a condition which will output (in the console or somewhere into the document) a recommendation on what to wear based on the value of the weather constant: if the weather is 'sunny' output Use sunscreen; if the weather is 'windy' output Wear a coat; if the weather is 'rainy' output Bring an umbrella; if the weather is anything else (other than the previous 3 options) print Dress accordingly.", id1)
 const btnWeather = document.getElementById(id1);
@@ -29,5 +29,21 @@ btnWeather.addEventListener("click", () => {
             break;
         default:
             phWeather.innerHTML = "Dress accordingly, you stupid."
+    }
+})
+
+///Whether odd
+const id2 = "odd"
+makeNewSectionWithButton("Whether odd", "Display user info, whether inserted number is odd or even.", id2)
+const btnOdd = document.getElementById(id2);
+const phOdd = document.querySelector(`.${id2}`);
+btnOdd.addEventListener("click", () => {
+    const number = Number(prompt("Insert the umber you want to check:"))
+    if (number % 2 === 0) {
+        phOdd.innerHTML = "The number is even."
+    } else if (number % 2 > 0) {
+        phOdd.innerHTML = "The number is odd."
+    } else {
+        phOdd.innerHTML = "Input is wrong."
     }
 })
