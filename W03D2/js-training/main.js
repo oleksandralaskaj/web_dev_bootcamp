0bba29f929c199d996069deba9ed3c11638974e4
oleksandralaskaj <alexandravalko@gmail.com>
2024-05-14 11:49:31 +0200

    weather redone with interface

--- W03D2/js-training/main.js
+++ W03D2/js-training/main.js
@@ -1,14 +1,33 @@
-const weather = prompt("What's the wether is like today?")
-
-switch (weather) {
-    case "sunny":
-        alert("Use sunscreen!");
-        break;
-    case "windy":
-        alert("Wea a coat.");
-        break;
-    case "rainy":
-        alert("Tak an umbrella!");
-        break;
-    default: alert("Dress accordingly, you stupid.")
+const body = document.querySelector("body")
+const makeNewSectionWithButton = (h1, p, id) => {
+    const section = document.createElement('section')
+    section.innerHTML = `<h1>${h1}</h1>
+        <p>${p}</p>
+        <button id=${id}>Click me to interact</button>
+        <p class="result ${id}"></p>`
+    body.appendChild(section)
 }
+
+///weather
+const id1 = "weather"
+makeNewSectionWithButton("Weather", "Write a condition which will output (in the console or somewhere into the document) a recommendation on what to wear based on the value of the weather constant: if the weather is 'sunny' output Use sunscreen; if the weather is 'windy' output Wear a coat; if the weather is 'rainy' output Bring an umbrella; if the weather is anything else (other than the previous 3 options) print Dress accordingly.", id1)
+const btnWeather = document.getElementById(id1);
+const phWeather = document.querySelector(`.${id1}`)
+btnWeather.addEventListener("click", () => {
+    const weather = prompt("What's the weather is like today?")
+    switch (weather) {
+        case "sunny":
+            phWeather.innerHTML = "Wear a sunscreen.";
+            break;
+        case "snowy":
+        case "stormy":
+        case "windy":
+            phWeather.innerHTML = "Wear a coat.";
+            break;
+        case "rainy":
+            phWeather.innerHTML = "Take an umbrella!";
+            break;
+        default:
+            phWeather.innerHTML = "Dress accordingly, you stupid."
+    }
+})
