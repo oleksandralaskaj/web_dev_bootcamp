2bedde18a75068394416b4cab60b18b0e6c75965
oleksandralaskaj <alexandravalko@gmail.com>
2024-05-14 10:30:16 +0200

    extended alphabet

--- W03D2/morning-workout/main.js
+++ W03D2/morning-workout/main.js
@@ -1,8 +1,8 @@
 const btn = document.querySelector("button");
-const letters = ["A", "B", "C", "D", "E", "F"];
+const letters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 btn.addEventListener("click", () => {
-    const oldNumber = Number(prompt("Insert your old seat number:"));
-    if (oldNumber >= 0 && oldNumber < 60) {
+    const oldNumber = Number(prompt("Insert your old seat number (0 to 259):"));
+    if (oldNumber >= 0 && oldNumber < 260) {
         const column = oldNumber % 10;
         const row = letters[Math.floor(oldNumber / 10)];
         alert(`A new seat number is ${row + column}.`);
