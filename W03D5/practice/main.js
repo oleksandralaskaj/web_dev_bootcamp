e4449effe70d9119edf55450b66498d974f9c5d6
oleksandralaskaj <alexandravalko@gmail.com>
2024-05-17 13:34:54 +0200

    small exercises done

--- W03D5/practice/main.js
+++ W03D5/practice/main.js
@@ -1,1 +1,20 @@
+const body = document.querySelector("body")
 
+const prgph = document.createElement("p")
+prgph.textContent = "lorem ipsum dolor sit amet"
+prgph.classList.add("main-p")
+prgph.style.color = "blue"
+
+body.appendChild(prgph)
+
+
+const elementFromHTML = (html) => {
+    const div = document.createElement('div');  // creates an element in the memory (not displayed)
+    div.innerHTML = html;
+    return div.firstChild;
+}
+
+const container = elementFromHTML(`<div class="container"><h4 class="subtitle">Subtitle</h4><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A animi asperiores beatae, consequuntur cupiditate delectus doloribus dolorum ea excepturi fugit pariatur provident quasi qui quo quos rerum sit ut voluptatum.</p></div>`);
+
+
+document.body.appendChild(container)
