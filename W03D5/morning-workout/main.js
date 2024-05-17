244dd07b5ef0f534e2b61dce43e37762625a5b02
oleksandralaskaj <alexandravalko@gmail.com>
2024-05-17 09:34:25 +0200

    while mouse is over lock, text is displayed. by clicking n the lock, text displayes permenantly, until lock is clicked again.

--- W03D5/morning-workout/main.js
+++ W03D5/morning-workout/main.js
@@ -1,1 +1,19 @@
+const text = document.querySelector(".text")
+const lock = document.querySelector(".padlock")
+let isLocked = false;
+
+lock.addEventListener("mouseenter", () => {
+    if (isLocked === false) {
+        text.classList.toggle("text_visible")
+    }
+})
+lock.addEventListener("mouseleave", () => {
+    if (isLocked === false) {
+        text.classList.toggle("text_visible")
+    }
+})
+lock.addEventListener("click", () => {
+    isLocked = !isLocked;
+    lock.classList.toggle("padlock_clicked")
+})
 
