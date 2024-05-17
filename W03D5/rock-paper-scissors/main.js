622282dc950037e324f378b813fdf45402286ecc
oleksandralaskaj <alexandravalko@gmail.com>
2024-05-17 16:40:15 +0200

    updated HTML, connected HTML and JS

--- W03D5/rock-paper-scissors/main.js
+++ W03D5/rock-paper-scissors/main.js
@@ -1,1 +1,11 @@
-
+//scoreboard display fields
+const phScoreHuman = document.querySelector(".score-human")
+const phScoreBot = document.querySelector(".score-bot")
+//scoreboard results of round display field
+const phChoiseHuman = document.querySelector(".choice-human")
+const phChoiseBot = document.querySelector(".choice-bot")
+const phResultOfRound = document.querySelector(".result-current")
+//buttons of players choise
+const btnRock = document.querySelector(".rock")
+const btnPaper = document.querySelector(".paper")
+const btnScissors = document.querySelector(".scissors")
