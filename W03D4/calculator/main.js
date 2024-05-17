a4080eb5bf85d8a216001a7205e7f81440d80880
oleksandralaskaj <alexandravalko@gmail.com>
2024-05-17 15:49:55 +0200

    a few changes to comment and formating of document

--- W03D4/calculator/main.js
+++ W03D4/calculator/main.js
@@ -1,1 +1,10 @@
+const num1 = ""
+const num2 = ""
+const result = 0
 
+const btnsOperations = document.querySelectorAll(".operations")
+btnsOperations.forEach((btn),()=>{
+    btn.addEventListener("click", () => {
+        btn.innerHTML
+    })
+})
