03d608bc335cf62498913e162be2901454a33f91
oleksandralaskaj <alexandravalko@gmail.com>
2024-05-21 16:27:21 +0200

    done: display of departure data with loading delay

--- W04D2/departures-with-delay/main.js
+++ W04D2/departures-with-delay/main.js
@@ -1,1 +1,34 @@
+const loadingScreen = document.querySelector(".loading")
+const phDeparturesList = document.querySelector(".departures-list")
+
+const hideElementToggle = (element) => {
+    element.classList.toggle("hidden")
+}
+
+const loadData = async (url) => {
+    const res = await fetch(url)
+    return res.json()
+}
+
+document.addEventListener("DOMContentLoaded", async () => {
+    const arrDepartures = await loadData("https://classes.codingbootcamp.cz/assets/classes/api/departures-slow.php")
+
+    arrDepartures.forEach(dep => {
+        const tableRow = document.createElement("tr")
+        tableRow.innerHTML = `
+                <td>${dep.time.hrs}:${dep.time.mins}</td>
+                <td>${dep.train}</td>
+                <td>${dep.no}</td>
+                <td>${dep.to}</td>
+                <td>${dep.status}</td>
+                <td>${dep.track}</td>`
+        phDeparturesList.appendChild(tableRow)
+    })
+
+    hideElementToggle(phDeparturesList)
+    hideElementToggle(loadingScreen)
+})
+
+
+
 
