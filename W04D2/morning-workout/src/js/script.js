c0ce2cfcf2b72eece322c126507480138f9b5baf
oleksandralaskaj <alexandravalko@gmail.com>
2024-05-21 09:07:33 +0200

    initial commit

---
+++ W04D2/morning-workout/src/js/script.js
@@ -0,0 +1,15 @@
+import { ProgressBar } from './ProgressBar';
+
+// @TODO no.1
+//    create new instance of ProgressBar.
+//    Explore ProgressBar class, but the constructor does not need any parameter.
+//    Take a body element (document.body) and append progress bar element to it.
+//    Progress bar element was created in the constructor of PB
+//    and it is available as a property "element" of the object you created.
+//
+//    code here:
+
+
+
+// @TODO no.2 once you have done TODO no.1 and you can see the progress bar,
+//    fix missing icon on plus button
