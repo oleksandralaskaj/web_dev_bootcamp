813e7097de889dc63eb3ddfc04aeb5a04547de6f
oleksandralaskaj <alexandravalko@gmail.com>
2024-05-22 17:15:42 +0200

    add custom script for adding section to the HTML

---
+++ examples/vite+js/scripts/SectionInBody.js
@@ -0,0 +1,32 @@
+export class SectionInBody {
+    constructor(id, title, description, hasButton) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.hasButton = hasButton
+    }
+
+    add() {
+        const section = document.createElement("section")
+        section.setAttribute("id", this.id)
+        section.innerHTML = `
+                        <h1>${this.title}</h1>
+                        <p>${this.description}</p>
+                        <div class="result result__${this.id}"></div>`
+        if (this.hasButton) {
+            const button = document.createElement("button")
+            button.setAttribute("id", `btn__${this.id}`)
+            button.innerHTML= "Click me to interact"
+            section.insertBefore(button, document.querySelector(`result__${this.id}`))
+        }
+        document.body.appendChild(section)
+    }
+
+    addInteraction(callback) {
+        document.getElementById(`btn__${this.id}`).addEventListener("click", callback)
+    }
+
+    printResult(content) {
+        document.querySelector(`.result__${this.id}`).innerHTML = content
+    }
+}
