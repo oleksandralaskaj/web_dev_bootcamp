5d3c6eba171be7bbb45a92fbceeab1d902cdd30f
oleksandralaskaj <alexandravalko@gmail.com>
2024-05-23 09:35:22 +0200

    done small rating text plus stars

--- W04D4/morning-workout/script.js
+++ W04D4/morning-workout/script.js
@@ -1,16 +1,22 @@
-const textRating = document.querySelector('.rating__value').textContent
+let textRating = document.querySelector('.rating__value')
 const stars = document.querySelectorAll('.rating__star')
 
 stars.forEach((star, index) => {
     star.addEventListener('click', (e) => {
-        console.log('The stars NodeList:')
-        console.log(stars)
-        console.log('The clicked star element:')
-        console.log(star)
-        console.log('The index of the clicked star in the NodeList:')
-        console.log(index)
-        console.log('Rating expressed by text:')
-        console.log(textRating)
-        console.log('-------------------------------------------------------------------------')
+        const rating = index + 1
+        resetText(textRating, rating)
+        resetStarsColor(stars, rating)
     })
 })
+
+const resetText = (field, rating) => {
+    field.textContent = rating
+}
+
+const resetStarsColor = (arr, rating) => {
+    arr.forEach((star) => star.classList.remove("rating__star--on"))
+    for (let i = 0; i < rating; i++) {
+        arr[i].classList.add("rating__star--on")
+    }
+}
+
