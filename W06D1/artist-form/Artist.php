1f54da02848f5028ad47ee24190f5ac1f7e06c83
oleksandralaskaj <alexandravalko@gmail.com>
2024-06-03 14:05:36 +0200

    initial commit

---
+++ W06D1/artist-form/Artist.php
@@ -0,0 +1,10 @@
+<?php
+
+class Artist
+{
+    public ?int $id = null;
+    public ?string $first_name = null;
+    public ?string $last_name = null;
+    public ?string $year_of_birth = null;
+    public ?string $genre = null;
+}
