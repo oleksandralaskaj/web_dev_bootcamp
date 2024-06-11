1a2e1c88ee58e230e29f25d3da2a49ff0bfc5405
oleksandralaskaj <alexandravalko@gmail.com>
2024-06-11 17:19:43 +0200

    add offeree model and homepage controller

--- W07D2/laravel-practice/app/Http/Controllers/HomepageController.php
+++ W07D2/laravel-practice/app/Http/Controllers/HomepageController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Offer;
+use App\Models\Offeree;
 use Illuminate\Support\Facades\DB;
 
 class HomepageController extends Controller
@@ -13,7 +14,11 @@ class HomepageController extends Controller
 //        $new_offer_id = DB::table('offers')
 //            ->insertGetId([1, 'Offer number one from employer number one.']);
         $offer = Offer::find(1);
-        $employer = $offer->offeree->name;
-        return view('index', compact('employer'));
+        $employer_name = $offer->offeree->name;
+        $employer_id = $offer->offeree->id;
+        $employer = Offeree::find($employer_id);
+        $offers_of_employer =$employer->offers;
+        dd($offers_of_employer);
+        return view('index', compact('employer_name'));
     }
 }
--- W07D2/laravel-practice/app/Models/Offeree.php
+++ W07D2/laravel-practice/app/Models/Offeree.php
@@ -4,14 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use App\Models\Offer;
 
 class Offeree extends Model
 {
     use HasFactory;
 
-    public function offers()
+    public function offers(): HasMany
     {
-        return $this->hasMany(Offer::class);
+        return $this->hasMany(Offer::class, 'offeree_id', "id");
     }
 }
