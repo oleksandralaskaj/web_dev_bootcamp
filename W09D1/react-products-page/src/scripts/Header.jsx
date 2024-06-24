c5f3b6395c4d6415f4bc47f7865a05ff295d3a74
oleksandralaskaj <alexandravalko@gmail.com>
2024-06-24 16:17:04 +0200

    working component import

---
+++ W09D1/react-products-page/src/scripts/Header.jsx
@@ -0,0 +1,22 @@
+
+export function Header() {
+    return (
+        <header>
+            <img src="https://classes.codingbootcamp.cz/assets/classes/workouts/aperture.png" alt="Aperture science"
+                 className="logo"/>
+
+                <div className="user">
+                    <span>chell@aperture.sc</span><br/>
+                    <a href="#">logout</a>
+                </div>
+
+                <nav>
+                    <a href="#">Home</a>
+                    <a href="#" className="current">Products</a>
+                    <a href="#">Stores</a>
+                    <a href="#">Customers</a>
+                </nav>
+        </header>
+
+)
+}
