161b613e267e786b58b06eadba383d3a5c91a6ba
oleksandralaskaj <alexandravalko@gmail.com>
2024-06-26 19:50:44 +0200

    random joke display app

--- W09D2/joke/src/App.jsx
+++ W09D2/joke/src/App.jsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
+import {useEffect, useState} from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [setup, setSetup] = useState('')
+    const [delivery, setDelivery] = useState('')
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const getJoke = async () => {
+        const res = await fetch('https://v2.jokeapi.dev/joke/Any')
+        const data = await res.json()
+        if (data.type === 'twopart') {
+            setSetup(data.setup)
+            setDelivery('')
+            setTimeout(() => setDelivery(data.delivery), 1000)
+        } else {
+            setSetup(data.joke)
+            setDelivery('')
+        }
+
+    }
+
+    useEffect(() => {
+        getJoke();
+    }, []);
+
+
+    return (
+        <>
+            <p>{setup}</p>
+            <p>{delivery}</p>
+            <button onClick={getJoke}>New joke</button>
+        </>)
 }
 
 export default App
